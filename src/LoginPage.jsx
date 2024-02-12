6c440bf6bb2077f7484d5c799a381045427bb4ae
swati <swatityagi8053@gmail.com>
2024-02-12 19:05:34 +0530

    start validation

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -3,6 +3,9 @@ import { loginUser } from "./redux/cardSlice";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { LoginresetError } from "./redux/authReducer";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
 import {
   TextField,
   Stack,
@@ -16,6 +19,31 @@ import { useNavigate } from "react-router-dom";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import { useState } from "react";
+
+const validationSchema = Yup.object({
+  username: Yup.string().required("*Username or Email is required"),
+  password: Yup.string().required("*Password is required"),
+});
+
+function MyField(props) {
+  const { label, ...otherProps } = props;
+
+  return (
+    <TextField
+      {...otherProps}
+      InputProps={{
+        style: { color: "black" },
+        inputProps: {
+          style: { color: "black" },
+        },
+      }}
+      InputLabelProps={{
+        style: { color: "Black" },
+      }}
+      label={label}
+    />
+  );
+}
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -67,81 +95,110 @@ const LoginPage = () => {
           boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
         }}
       >
-        <Stack>
-          <Typography variant="h4">Login Form</Typography>
+        <Typography variant="h4">Login Form</Typography>
+        <Formik
+          initialValues={loginCredentials}
+          validationSchema={validationSchema}
+          onSubmit={handleLogin}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <Stack>
+                <Field
+                  as={MyField}
+                  id="outlined-basic"
+                  label="Username or Email"
+                  name="username"
+                  variant="outlined"
+                  margin="normal"
+                  placeholder="Enter Username or Email..."
+                  value={loginCredentials.username}
+                  onChange={(e) =>
+                    setLoginCredentials({
+                      ...loginCredentials,
+                      username: e.target.value,
+                    })
+                  }
+                />
 
-          <TextField
-            id="outlined-basic"
-            label="Username or Email"
-            variant="outlined"
-            margin="normal"
-            placeholder="Enter Username or Email..."
-            value={loginCredentials.username}
-            onChange={(e) =>
-              setLoginCredentials({
-                ...loginCredentials,
-                username: e.target.value,
-              })
-            }
-          />
+                {errors.username &&
+                touched.username &&
+                loginCredentials.username === "" ? (
+                  <div style={{ color: "red" }}>{errors.username}</div>
+                ) : null}
 
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            margin="normal"
-            placeholder="Enter your password..."
-            type={showPassword ? "text" : "password"}
-            value={loginCredentials.password}
-            onChange={(e) =>
-              setLoginCredentials({
-                ...loginCredentials,
-                password: e.target.value,
-              })
-            }
-          />
+                <Field
+                  as={MyField}
+                  id="outlined-basic"
+                  label="Password"
+                  name="password"
+                  variant="outlined"
+                  margin="normal"
+                  placeholder="Enter your password..."
+                  type={showPassword ? "text" : "password"}
+                  value={loginCredentials.password}
+                  onChange={(e) =>
+                    setLoginCredentials({
+                      ...loginCredentials,
+                      password: e.target.value,
+                    })
+                  }
+                />
 
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="flex-start"
-            mt={1}
-            mb={1}
-          >
-            <IconButton
-              onClick={handleTogglePassword}
-              sx={{ color: "Highlight" }}
-            >
-              {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-            </IconButton>
-            <Typography sx={{ pr: 1, color: "Highlight" }}>
-              {showPassword ? "Hide" : "Show"} Password
-            </Typography>
-          </Stack>
+                {errors.password &&
+                touched.password &&
+                loginCredentials.password === "" ? (
+                  <div style={{ color: "red" }}>{errors.password}</div>
+                ) : null}
 
-          <Button variant="contained" color="primary" onClick={handleLogin}>
-            Login
-          </Button>
-          <Container
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              mt: 3,
-              mb: 5,
-              gap: 9,
-            }}
-          >
-            <Link href="#" underline="hover">
-              {"Forgot Password?"}
-            </Link>
-            <span>
-              <span>Don't have an account?</span>
-              <Link to="/signup" underline="hover">
-                {"Sign up"}
-              </Link>
-            </span>
-          </Container>
-        </Stack>
+                <Stack
+                  direction="row"
+                  alignItems="center"
+                  justifyContent="flex-start"
+                  mt={1}
+                  mb={1}
+                >
+                  <IconButton
+                    onClick={handleTogglePassword}
+                    sx={{ color: "Highlight" }}
+                  >
+                    {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                  </IconButton>
+                  <Typography sx={{ pr: 1, color: "Highlight" }}>
+                    {showPassword ? "Hide" : "Show"} Password
+                  </Typography>
+                </Stack>
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleLogin}
+                >
+                  Login
+                </Button>
+                <Container
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    mt: 3,
+                    mb: 5,
+                    gap: 9,
+                  }}
+                >
+                  <Link href="#" underline="hover">
+                    {"Forgot Password?"}
+                  </Link>
+                  <span>
+                    <span>Don't have an account?</span>
+                    <Link to="/signup" underline="hover">
+                      {"Sign up"}
+                    </Link>
+                  </span>
+                </Container>
+              </Stack>
+            </Form>
+          )}
+        </Formik>
       </Container>
     </Container>
   );
--- src/SignupPage.jsx
+++ src/SignupPage.jsx
@@ -2,9 +2,9 @@ import { useDispatch } from "react-redux";
 import { signupUser } from "./redux/cardSlice";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import {Formik, Form} from "formik";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 import {
-  TextField,
   Stack,
   Typography,
   Container,
@@ -12,11 +12,32 @@ import {
   Select,
   MenuItem,
   InputLabel,
+  TextField,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { resetError } from "./redux/authReducer";
+
+function MyField(props) {
+  const { label, ...otherProps } = props;
+
+  return (
+    <TextField
+      {...otherProps}
+      InputProps={{
+        style: { color: "black" },
+        inputProps: {
+          style: { color: "black" },
+        },
+      }}
+      InputLabelProps={{
+        style: { color: "Black" },
+      }}
+      label={label}
+    />
+  );
+}
 
 const SignupPage = () => {
   const navigate = useNavigate();
@@ -37,6 +58,16 @@ const SignupPage = () => {
     role: "guest",
     password: "",
     confirmPassword: "",
+  });
+
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .required("Password is required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Confirm Password is required"),
   });
 
   const handleSignup = () => {
@@ -69,81 +100,119 @@ const SignupPage = () => {
           boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
         }}
       >
-        <Stack>
-          <Typography variant="h4">Sign Up Form</Typography>
+        <Typography variant="h4">Sign Up Form</Typography>
 
-          <TextField
-            id="outlined-basic"
-            label="Username"
-            variant="outlined"
-            margin="normal"
-            placeholder="Enter your name..."
-            value={signupData.username}
-            onChange={(e) =>
-              setSignupData({ ...signupData, username: e.target.value })
-            }
-          />
+        <Formik
+          initialValues={signupData}
+          validationSchema={validationSchema}
+          onSubmit={handleSignup}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <Stack>
+                <Field
+                  as={MyField}
+                  id="outlined-basic"
+                  name="username"
+                  label="Username"
+                  variant="outlined"
+                  margin="normal"
+                  placeholder="Enter your name..."
+                  value={signupData.username}
+                  onChange={(e) =>
+                    setSignupData({ ...signupData, username: e.target.value })
+                  }
+                />
 
-          <InputLabel htmlFor="role" sx={{ mt: 2, textAlign: "left", ml: 1 }}>
-            Role
-          </InputLabel>
+                {errors.username &&
+                touched.username &&
+                signupData.username === "" ? (
+                  <div style={{ color: "red" }}>{errors.username}</div>
+                ) : null}
 
-          <Select
-            sx={{ textAlign: "left" }}
-            htmlFor="Role"
-            id="role"
-            value={signupData.role}
-            onChange={(e) =>
-              setSignupData({ ...signupData, role: e.target.value })
-            }
-            variant="outlined"
-            margin="normal"
-          >
-            <MenuItem value="guest">Guest</MenuItem>
-            <MenuItem value="admin">Admin</MenuItem>
-          </Select>
+                <InputLabel
+                  htmlFor="role"
+                  sx={{ mt: 2, textAlign: "left", ml: 1 }}
+                >
+                  Role
+                </InputLabel>
 
-          <TextField
-            id="outlined-basic"
-            label="Password"
-            variant="outlined"
-            margin="normal"
-            placeholder="Enter your password..."
-            type="password"
-            value={signupData.password}
-            onChange={(e) =>
-              setSignupData({ ...signupData, password: e.target.value })
-            }
-          />
-          <TextField
-            id="outlined-basic"
-            label="Confirm Password"
-            variant="outlined"
-            margin="normal"
-            placeholder="Enter your password..."
-            type="password"
-            value={signupData.confirmPassword}
-            onChange={(e) =>
-              setSignupData({
-                ...signupData,
-                confirmPassword: e.target.value,
-              })
-            }
-          />
+                <Select
+                  sx={{ textAlign: "left" }}
+                  id="role"
+                  value={signupData.role}
+                  onChange={(e) =>
+                    setSignupData({ ...signupData, role: e.target.value })
+                  }
+                  variant="outlined"
+                  margin="normal"
+                >
+                  <MenuItem value="guest">Guest</MenuItem>
+                  <MenuItem value="admin">Admin</MenuItem>
+                </Select>
 
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{ mb: 3 }}
-            onClick={() => handleSignup()}
-          >
-            Sign up
-          </Button>
-          <span>
-            Already have an account?
-            <Link to="/">{"Login"}</Link>
-          </span>
-        </Stack>
+                <Field
+                  as={MyField}
+                  id="outlined-basic"
+                  label="Password"
+                  variant="outlined"
+                  name="password"
+                  margin="normal"
+                  placeholder="Enter your password..."
+                  type="password"
+                  value={signupData.password}
+                  onChange={(e) =>
+                    setSignupData({ ...signupData, password: e.target.value })
+                  }
+                />
+
+                {errors.password &&
+                touched.password &&
+                signupData.password === "" ? (
+                  <div style={{ color: "red" }}>{errors.password}</div>
+                ) : null}
+
+                <Field
+                  as={MyField}
+                  id="outlined-basic"
+                  label="Confirm Password"
+                  name="confirmPassword"
+                  variant="outlined"
+                  margin="normal"
+                  placeholder="Enter your password..."
+                  type="password"
+                  value={signupData.confirmPassword}
+                  onChange={(e) =>
+                    setSignupData({
+                      ...signupData,
+                      confirmPassword: e.target.value,
+                    })
+                  }
+                />
+
+                {errors.confirmPassword &&
+                touched.confirmPassword &&
+                signupData.confirmPassword === "" ? (
+                  <div style={{ color: "red" }}>{errors.confirmPassword}</div>
+                ) : null}
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{ mb: 3 }}
+                  onClick={() => handleSignup()}
+                  type="submit"
+                >
+                  Sign up
+                </Button>
+                <span>
+                  Already have an account?
+                  <Link to="/">{"Login"}</Link>
+                </span>
+              </Stack>
+            </Form>
+          )}
+        </Formik>
       </Container>
     </Container>
   );
